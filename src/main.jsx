cfce773f3e9091353918caf634125afd7525942e
Cristian Villa <c.villavirgen@gmail.com>
2023-08-22 09:56:25 -0600

    Create & Import CSS file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import TodoApp from '@/components/TodoApp';
+import '@/styles/app.css';
 
 const domContainer = document.getElementById("root");
 const root = ReactDOM.createRoot(domContainer);
