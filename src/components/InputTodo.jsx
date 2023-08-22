3c57fa59453ab53e195c610f20ba5dcdcfefa01f
Cristian Villa <c.villavirgen@gmail.com>
2023-08-21 18:55:36 -0600

    Implement adding new todo items with InputTodo component

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,4 +1,33 @@
-const InputTodo = () => {
-    return <div>input field here...</div>;
+import React, { useState } from 'react';
+
+const InputTodo = ({ addTodoItem }) => {
+    const [title, setTitle] = useState('');
+
+    const handleChange = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (title.trim()) {
+            addTodoItem(title);
+            setTitle('');
+        } else {
+            alert('Please add item');
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Add Todo..."
+                value={title}
+                onChange={handleChange}
+            />
+            <button type="submit">Submit</button>
+        </form>
+    );
 };
+
 export default InputTodo;
